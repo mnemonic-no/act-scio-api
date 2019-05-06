806ce1175285d8ccc9bd0fe6a182bc70d2e2d059
Geir Skjotskift <geir@mnemonic.no>
2019-05-06 13:49:54 +0200

    fixed failing test, removed old debug statemenet (#5)

--- test/scio_api/core_test.clj
+++ test/scio_api/core_test.clj
@@ -6,17 +6,15 @@
 (deftest test-upload
   (System/setProperty "*scio-api-ini*" "etc/sample.ini")
   (is (= {:status 200
-          :bytes 4
-          :body "ok"
-          :headers {}}
+          :body "{\"error\":null,\"filename\":\"/tmp/test.txt\",\"bytes\":4}"
+          :headers {"Content-Type" "application/json; charset=utf-8"}}
          (api (-> (mock/request :post "/submit")
                   (mock/json-body {:filename "test.txt"
                                    :content "VGVzdA=="})))))
   (is (= {:status 200
-          :bytes 4
-          :body "ok"
-          :headers {}}
+          :body "{\"error\":null,\"filename\":\"/tmp/evil.txt\",\"bytes\":4}"
+          :headers {"Content-Type" "application/json; charset=utf-8"}}
          (api (-> (mock/request :post "/submit")
                   (mock/json-body {:filename "../../../../../../../../../../../../../../../root/evil.txt"
                                    :content "VGVzdA=="}))))))
- 
+
